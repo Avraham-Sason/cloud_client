0d7fb59e9d1b855a1369ffb687adeaab33ba3d50
שגיא בוטה <sagibuta@gmail.com>
2023-08-11 00:38:15 +0300

    navitem...stil not work

--- src/layout/MainLayout/index.jsx
+++ src/layout/MainLayout/index.jsx
@@ -10,7 +10,7 @@ export default function MainLayout() {
             <Header />
             <Content />
             <div>
-                banana
+                
             </div>
         </>
     )
