c54ec377a158e5685795cb1edeea9f71bb96f334
avi1998s <avi6190i@gmail.com>
2023-08-11 00:35:32 +0300

    .

--- src/layout/MainLayout/index.jsx
+++ src/layout/MainLayout/index.jsx
@@ -9,7 +9,9 @@ export default function MainLayout() {
         <>
             <Header />
             <Content />
-            
+            <div>
+                banana
+            </div>
         </>
     )
 }
